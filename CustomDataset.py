30e39f87e3092a3344eaacbcfeceab2f1aea9751
Brendan Raines <brendan.raines@gmail.com>
2025-01-25 23:55:28 +1100

    update and test CustomDataset

--- CustomDataset.py
+++ CustomDataset.py
@@ -3,7 +3,7 @@ import torch
 from torch.utils.data import Dataset
 import csv
 
-class customerDataset(Dataset):
+class CustomDataset(Dataset):
     def __init__(self, images_path:str):
         self.labels = []
         self.images = []
@@ -19,14 +19,15 @@ class customerDataset(Dataset):
     def __len__(self) -> int:
         return len(self.labels)
     
-    def __getitem__(self, idx:int) -> tuple(int, np.array):
+    def __getitem__(self, idx:int) -> tuple[int, np.array]:
         image = self.images[idx]
         temp_image = []
         temp_row = []
         for i in range(1,len(image)+1):
-            temp_row.append(image[i-1])
+            temp_row.append(int(image[i-1]))
             if not i%28:
                 temp_image.append(temp_row)
                 temp_row = []
+        
 
-        return self.labels[idx], temp_image
+        return torch.tensor(int(self.labels[idx])), torch.from_numpy(np.array(temp_image))
